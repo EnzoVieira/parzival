898503a5cb900a48f215df487ab02719d593f18c
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-05-26 20:01:23 +0100

    Add curriculum form

--- lib/parzival_web/config.ex
+++ lib/parzival_web/config.ex
@@ -89,6 +89,12 @@ defmodule ParzivalWeb.Config do
         tabs: []
       },
       %{
+        key: :leaderboard,
+        title: "Leaderboard",
+        url: Routes.leaderboard_index_path(conn, :index),
+        tabs: []
+      },
+      %{
         key: :jobs,
         title: "Jobs",
         url: Routes.offer_index_path(conn, :index),
